70c4b5d6b920ca00a485b70e6d6401ad7e44e297
sashka <sashkacrab@gmail.com>
2016-07-17 18:35:29 +0300

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var jade = require('gulp-jade');
 var rjs = require('gulp-requirejs');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
 // add required packages
 
 gulp.task('connect', function() {
@@ -20,11 +22,13 @@ gulp.task('jade', function() {
 });
 
 gulp.task('sass', function() {
-	// implement sass task
+	gulp.src('src/sass/*.sass')
+		.pipe(sass())
+		.pipe(gulp.dest('dist/css'))
+		.pipe(connect.reload());
 });
 
 gulp.task('requireJS', function() {
-	// implement bundle.js file uglification
 	rjs({
 		baseUrl: 'src/js',
 		name: '../../node_modules/almond/almond',
@@ -33,13 +37,15 @@ gulp.task('requireJS', function() {
 		out: 'bundle.js',
 		wrap: true
 	})
+	.pipe(uglify())
 	.pipe(gulp.dest('dist/js'))
 	.pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
 	gulp.watch('src/jade/*.jade', ['jade']);
-	// add watch for .sass and .js files
+	gulp.watch('src/sass/*.sass', ['sass']);
+	gulp.watch('src/js/*.js', ['requireJS']);
 });
 
 gulp.task('default', ['requireJS', 'jade', 'sass', 'connect', 'watch']);
